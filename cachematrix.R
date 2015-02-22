896b24e3d3af8d6ffd47960cff924ce5f53bedd8
fwerrij <frans@werrij.nl>
2015-02-22 19:23:25 +0100

    comments and indents

--- cachematrix.R
+++ cachematrix.R
@@ -15,19 +15,20 @@
 ## 4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <‐ NULL
-  set <‐ function(y) {
-## In the next two lines the <<- operator is used to assign a value to an object in
-## an environment that is different from the current environment.
-    x <<‐ y
-    m <<‐ NULL
-  }
-  get <‐ function() x
-  setsolve <‐ function(solve) m <<‐ solve
-  getsolve <‐ function() m
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+    m <‐ NULL
+    set <‐ function(y) {
+        ## In the next two lines the <<- operator is used to assign a value to 
+        ## an object in an environment that is different from the current 
+        ## environment.
+        x <<‐ y
+        m <<‐ NULL
+    }
+    get <‐ function() x
+    setsolve <‐ function(solve) m <<‐ solve
+    getsolve <‐ function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 ## The following function calculates the inverse of the special "vector" created 
@@ -38,18 +39,27 @@ makeCacheMatrix <- function(x = matrix()) {
 ## inverse in the cache via the setsolve function.
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  m <‐ x$getsolve()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-## initialize matrix "A"  
-  data <‐ x$get()
-  ## create coresponding unity matrix E
-  e <- diag(1,nrow(data))
-## calculate the inverse of A by solving the equation AX=E
-  m <‐ solve(data,e, ...)
-  x$setsolve(m)
-  m
-  }
+    ## Return a matrix that is the inverse of 'x'
+    m <‐ x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    ## initialize matrix "A"  
+    data <‐ x$get()
+    ## create coresponding unity matrix E
+    e <- diag(1,nrow(data))
+    ## calculate the inverse of A by solving the equation AX=E
+    m <‐ solve(data,e, ...)
+    x$setsolve(m)
+    m
+}
+## Tests for first calculating and afterwards retreving from cache for 
+## testmatrices:
+## A1 = 1 3 2 4 (2 x 2)             giving        
+##          inv(A1) = -2 1.4 1 -0.5
+## A2 = 1 0 1 2 4 0 3 5 6 (3 x 3)   giving 
+##          inv(A2) = 1.091 0.227 -0.182 -0.545 0.136 0.091 -0.091 -0.227 0.182 
+## A3 = 1 0 5 2 1 6 3 4 0 (3 x 3)   giving 
+##          inv(A3) = -24 20 -5 18 -15 4 5 -4 1
+## went according to expectations.
